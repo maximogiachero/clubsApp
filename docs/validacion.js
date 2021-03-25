b44f4c32f481d07f8f0d6e4c9065a1a60a3d760c
fvgtekken <federicovongromann@gmail.com>
2021-03-25 19:48:18 -0300

    ultima modificacion del formulario

--- docs/validacion.js
+++ docs/validacion.js
@@ -3,46 +3,52 @@ var eleform = document.getElementById('mensajeForm')
 
 eleform.style.display='none'
 
+function callPhp(){
+let data = {nombres:'maxi'}
+    fetch('https://example.com/profile', { method: 'POST', 
+    headers: { 'Content-Type': 'application/json', }, 
+    body: JSON.stringify(data), }) 
+    .then(response => response.json()) 
+    .then(data => { console.log('Success:', data); }) 
+    .catch((error) => { console.error('Error:', error); });
+return false
+
+} 
 
 function validacion () {
-eleform.style.display='none'
-if(document.form.email.value===""){
-
-document.form.email.focus()
-eleform.style.display='inline'
-
-eleform.innerHTML='el campo email es requerido'
+    
+    eleform.style.display='none'
 
 
-return false
+    if(document.form.email.value===""){
+        document.form.email.focus()
+        eleform.style.display='inline'
+        eleform.innerHTML='el campo email es requerido'
+        return false
+    }else {
+        console.log(document.form.email.value)
 
- }
- if(document.form.nombre.value===""){
-
-document.form.nombre.focus()
-eleform.style.display='inline'
-
-eleform.innerHTML='el campo de nombre y apellido es requerido'
-
-
-return false
-
- }
-if(document.form.comentarios.value===""){
-
-document.form.comentarios.focus()
-eleform.style.display='inline'
-
-eleform.innerHTML='el comentario es requerido deja tu comentario'
+    }
 
 
-return false
+    if(document.form.nombre.value===""){
+        document.form.nombre.focus()
+        eleform.style.display='inline'
+        eleform.innerHTML='el campo de nombre y apellido es requerido'
+        return false
+    }else{
+        console.log(document.form.nombre.value)
+    }
 
-}
+    if(document.form.comentarios.value===""){
+        document.form.comentarios.focus()
+        eleform.style.display='inline'
+        eleform.innerHTML='el comentario es requerido deja tu comentario'
+        return false
+    }else{
+        console.log(document.form.comentarios.value)
+    }
 
-console.log ('soy la funcion validacion')
-return false
-
-
-
+    console.log ('soy la funcion validacion')
+    return false
 }
